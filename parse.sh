dd13407395a65d744b192d57f71f48a3b3e213d1
topologyYDM <topologyYDM@gmail.com>
2018-09-08 16:09:27 +0800

    cope with erroneous situations

--- parse.sh
+++ parse.sh
@@ -7,6 +7,8 @@ nodes=$(cat ${export_path} | jq ".configs")
 length=$(cat ${export_path} | jq ".configs | length")
 typeset -i length
 
+curl_web="baidu.com"
+
 out='out.txt'
 if [ ! -n $out ]; then
     touch $out
@@ -14,7 +16,6 @@ else
     rm $out
     touch $out
 fi
-export ALL_PROXY=socks5://127.0.0.1:1080
 
 for (( i=0; i<$length; i=i+1 ))
 do
@@ -46,27 +47,39 @@ do
     # declare -i local_port
     declare -i timeout
 
-    # echo $server
-    # echo $server_port
-    # echo $local_address
-    # echo $local_port
-    # echo $password
-    # echo $method
-    # echo $timeout
+#    echo $server
+#    echo $server_port
+#    echo $local_address
+#    echo $local_port
+#    echo $password
+#    echo $method
+#    echo $timeout
 
     # connect to VPS
     sslocal -s $server -p $server_port -l $local_port -k\
         $password -m $method -q &
-    echo "Connected no.${i}"
+    export ALL_PROXY=socks5://127.0.0.1:1080
+    sleep 0.5
 
-    # test proxy speed
-    echo $server >> $out
-    speedtest --simple >> $out
-    echo >> './out.txt'
+    if_connect="fail"
+    curl --connect-timeout 6 -m 8 -i $curl_web > /dev/null && if_connect="success"
 
-    # kill sslocal process
-    lsof -i:1080 | grep 'sslocal' | cut -d ' ' -f2 | xargs kill
-
-    echo "Finished no.${i}!"
+    if [ "$if_connect" = "success" ]; then
+        echo "Connected no.${i}"
+        echo $server >> $out
+        speedtest --simple >> $out
+        echo >> './out.txt'
+#        lsof -i:1080 | grep 'sslocal' | cut -d ' ' -f2 | xargs kill
+        pkill sslocal
+        echo "Finished no.${i}!"
+    elif [ "$if_connect" = "fail" ]; then
+        echo "Failed no.${i}"
+        echo $server >> $out
+        echo 'Failed' >> './out.txt'
+        echo >> './out.txt'
+#        lsof -i:1080 | grep 'sslocal' | cut -d ' ' -f2 | xargs kill
+        pkill sslocal
+    else
+        echo "unkown situation!"
+    fi
 done
-
